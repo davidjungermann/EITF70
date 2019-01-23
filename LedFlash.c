b8c70b1d7a5eeab70e060b9b40a0a35592a2d0ac
ravedave <dic15dju@student.lu.se>
2019-01-23 14:49:10 +0100

    Final

--- LedFlash.c
+++ LedFlash.c
@@ -1,15 +1,16 @@
-// Write a program that blinks a LED with the frequency of 1 Hz. Use the functions described in
+// Home assignment 6.1 Write a program that blinks a LED with the frequency of 1 Hz. Use the functions described in
 //Appendix A.1.
 
 #include <yoda.h>
 int main()
 {
     int num = 0;
-    void led_init();  // Initierar led-lampor
-    
-    while(1){
+    void led_init(); // Initierar led-lampor
+
+    while (1)
+    {
         void led_toggle(num); // togglar av och på
-        delay(1000); // väntar 1 sekund
+        _ms_delay(1000);      // väntar 1 sekund
     }
     return 0;
 }
