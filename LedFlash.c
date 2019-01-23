56a5b23932c25ef0768fd930941fffe3bd7b4b74
ravedave <dic15dju@student.lu.se>
2019-01-23 09:49:02 +0100

    Program to flash LED 1

---
+++ LedFlash.c
@@ -0,0 +1,19 @@
+// Write a program that blinks a LED with the frequency of 1 Hz. Use the functions described in
+//Appendix A.1.
+
+#include <yoda.h>
+int main()
+{
+    int num = 1;
+    void led_init();  // Initierar led-lampor
+    
+    while(1){
+        void led_on(num);
+        printf("LED on\n");
+        delay(1000);
+        void led_off(num);
+        printf("LED off\n");
+        delay(1000);
+    }
+    return 0;
+}
